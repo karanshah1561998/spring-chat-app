8f9b3050ae04a8e90e913d06cc14ef9b4b3a41b4
Karan Shah <karanshah1561998@gmail.com>
2025-03-24 14:04:17 -0400

    Initial commit: Spring Boot Chat App with Docker support

---
+++ src/main/java/com/project/chat/chat/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.project.chat.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
